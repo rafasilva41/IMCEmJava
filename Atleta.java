a3be0b7ceec794078235fad6d7f5024bb847fffc
rafasilva41 <162514981+rafasilva41@users.noreply.github.com>
2025-09-17 20:41:03 -0300

    Update Atleta.java

--- Atleta.java
+++ Atleta.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
-public class Atleta {
-    String esportePraticado;
+public class Atleta extends Pessoa {
+    private String esportePraticado;
     scanner.nextline();
 
+
+
     System.out.print("Voce eh um atleta com nivel de atividade alto? (sim/nao): ")
-    System.out.print("Voce eh um atleta com nivel de atividade alto? (sim/nao): ")
+            System.out.print("Voce eh um atleta com nivel de atividade alto? (sim/nao): ")
     String resposta = scanner.nextline().trim().toLowerCase();
 
     System.out.print("Qual esporte voce pratica?");
@@ -15,4 +17,13 @@ public class Atleta {
         imc *= 0.95;
         System.out.println("Aplicando ajuste de 5% para atleta de alta atividade.");
     }
+
+    public String getEsportePraticado() {
+        return esportePraticado;
+    }
+
+    public void setEsportePraticado(String esportePraticado) {
+        this.esportePraticado = esportePraticado;
+    }
 }
+
