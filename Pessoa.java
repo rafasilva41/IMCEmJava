982a3289fc610c7775b4260837df484f239939ce
rafasilva41 <162514981+rafasilva41@users.noreply.github.com>
2025-09-18 11:20:39 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,39 +1,16 @@
-import java.sql.SQLOutput;
-
 public class Pessoa {
     private String nome;
+    private double peso;
+    private double altura;
 
-    System.out.print("Digite seu nome: ");
-    private String nome = scanner.nextString();
-
-    System.out.print("Digite seu peso em kg: ");
-    private double peso = scanner.nextDouble();
-
-    System.out.print("Digite a sua altura em metros: ");
-    private double altura = scanner.nextDouble();
-
-    double imc = peso / (altura * altura);
-
-    System.out.printf("Resultado do IMC: \n" + imc);
-
-    if (imc < 18.5) {
-        System.out.println("Portanto, voce esta classificado como abaixo do peso.");
-    } else if (imc < 25){
-        System.out.println("Portanto, voce esta classificado como peso normal.");
-    } else if (imc < 30){
-        System.out.println("Portanto, voce esta classificado como sobrepeso.");
-    } else if (imc < 35){
-        System.out.println("Portanto, voce esta classificado como obesidade grau I.");
-    } else (imc < 40){
-        System.out.println("Portanto, voce esta classificado como obesidade grau II.");
-    } else {
-        System.out.println("Portanto, voce esta classificado como obesidade grau III (morbida).");
+    public Pessoa(String nome, double peso, double altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
     }
 
-    scanner.close();
-
     public String getNome() {
-    return.nome;
+        return nome;
     }
 
     public void setNome(String nome) {
@@ -41,19 +18,25 @@ public class Pessoa {
     }
 
     public Double getPeso() {
-        return.peso;
+        return peso;
     }
 
     public void setPeso(Double peso) {
-        this.peso = peso;
+            this.peso = peso;
     }
 
     public Double getAltura() {
-        return.altura;
+        return altura;
     }
 
-    public void setAltura(String altura) {
-        this.altura = altura;
+    public void setAltura(double altura) {
+            this.altura = altura;
+    }
+
+    public double IMC() {
+        double imc = peso / (altura * altura);
+        return Math.round(imc * 100) / 100;
     }
 }
+
 
