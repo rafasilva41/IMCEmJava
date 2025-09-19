196baa043de373c0d2630a0d0e33537597e4d874
rafasilva41 <162514981+rafasilva41@users.noreply.github.com>
2025-09-19 11:43:46 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     Pessoa atleta = new Atleta("Rebeca", 46, 1.55, "Ginastica");
 
-    System.out.println(pessoa.getNome() + " - IMC: " + pessoa.IMC());
-    System.out.println(atleta.getNome() + " (Atleta de " + ((Atleta) atleta).getEsportePraticado() + ") - IMC ajustado: " + atleta.IMC());
+    System.out.println(pessoa.getNome() + " - IMC: " + pessoa.calcularIMC());
+    System.out.println(atleta.getNome() + " (Atleta de " + ((Atleta) atleta).getEsportePraticado() + ") - IMC ajustado: " + atleta.calcularIMC());
         }
     }
